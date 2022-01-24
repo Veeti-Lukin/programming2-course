faece2665789b6840c88a085c060ca748e65f3a9
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-24 22:22:16 +0200

    task 2.3.1 add BOING and ZIP BOING print features

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 const int ZIP = 3;
+const int BOING = 7;
 
 int main()
 {
@@ -10,8 +11,14 @@ int main()
     std::cin >> number_amount;
 
     for (int i=1; i < number_amount+1; ++i) {
-        if (i % ZIP == 0) {
+        if (i % ZIP == 0 and i % BOING == 0){
+            std::cout << "zip boing" <<std::endl;
+        }
+        else if (i % ZIP == 0) {
             std::cout << "zip" << std::endl;
+        }
+        else if (i % BOING == 0) {
+            std::cout << "boing" << std::endl;
         }
         else {
             std::cout << i << std::endl;
