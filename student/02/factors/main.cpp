61f55542ee5d588e96a1edc1ab5fd01731c3294e
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-25 00:05:44 +0200

    task 2.3.2 add feature for fiding closest factors of the number

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -4,15 +4,36 @@ using namespace std;
 
 int main()
 {
-    int original_number = 0;
+    int number = 0;
 
     cout << "Enter a positive number: ";
-    cin >> original_number;
+    cin >> number;
 
-    if (original_number <= 0) {
+    if (number <= 0) {
         cout << "Only positive numbers accepted" << endl;
         return 0;
     }
 
+    int shortest_distance = number;
+    int factor1, factor2;
+
+    for (int i=1; i < number; i++) {
+       int n = number / i;
+
+       if (n * i == number) {
+           int distance = n - i;
+
+           if (distance < 0)
+               distance = - distance;
+
+           if (distance < shortest_distance) {
+               shortest_distance = distance;
+               factor1 = i;
+               factor2 = n;
+           }
+
+       }
+    }
+    cout << number << "=" << factor1 << "*" << factor2 << endl;
     return 0;
 }
