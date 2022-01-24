d910da37e5bdb4855efcb840de95f7fa422b9444
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-24 23:31:51 +0200

    task 2.3.2 add feature for user input and error management

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
+    int original_number = 0;
+
     cout << "Enter a positive number: ";
+    cin >> original_number;
+
+    if (original_number <= 0) {
+        cout << "Only positive numbers accepted" << endl;
+        return 0;
+    }
 
     return 0;
 }
