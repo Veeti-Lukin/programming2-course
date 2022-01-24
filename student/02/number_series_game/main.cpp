663bb317c3054d382d00feba4af9918169cd1a21
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-24 21:37:17 +0200

    task 2.3.1 add user input and number printing

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    int number_amount = 0;
 
+    std::cout << "How many numbers would you like to have? ";
+    std::cin >> number_amount;
+
+    for (int i=0; i < number_amount+1; ++i) {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
