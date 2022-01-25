2f69da1bcfba6af817ad8638ab4e459d081e1533
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-25 11:42:34 +0200

    TASK 2.4.1 ADD FEATURE: for user input

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int number = 0;
+    int cubed_number = 0;
+
+    std::cout << "Enter a number: " << std::endl;
+    std::cin >> number;
+
+    return 0;
+}
