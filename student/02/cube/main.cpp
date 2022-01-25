f8bfac0e5f7f97599747a0b3046306fda4803016
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-25 12:00:25 +0200

    TASK 2.4.1 ADD FEATURE: for printing cubed number and cheking if it's right

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -5,8 +5,16 @@ int main()
     int number = 0;
     int cubed_number = 0;
 
-    std::cout << "Enter a number: " << std::endl;
+    std::cout << "Enter a number: ";
     std::cin >> number;
 
+    cubed_number = number * number * number;
+
+    if (cubed_number < 0 or (cubed_number/number/number) != number) {
+        std::cout << "ERROR! The cube of " << number << " is not " << cubed_number << "." << std::endl;
+        return 0;
+    }
+
+    std::cout << "The cube of " << number << " is " << cubed_number << "." << std::endl;
     return 0;
 }
