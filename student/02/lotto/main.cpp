4aa44809e50ee92f37605564377a41a05281dc39
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-25 16:36:37 +0200

    TASK 2.5.1 ADD FEATURE: for finding out amount of combinations and printing probability

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -1,28 +1,46 @@
-// Example program
 #include <iostream>
-#include <string>
 
-long int factorial (int number) {
-    long int number_factorial = 0;
+using namespace std;
+
+long int factorial(int number) {
+    long int number_factorial = 1;
 
     if (number == 0) {
         return 1;
     }
 
-    for (int i = 1; i < number; --i) {
+    for (int i = number; i > 0; --i) {
         number_factorial *= i;
-
     }
 
     return number_factorial;
 }
 
+long int all_combinations(int total, int drawn) {
+    return factorial(total) / (factorial(total - drawn) * factorial(drawn));
+}
+
 int main()
 {
-    while (true) {
-        int number = 0;
-        std::cin >> number;
-        std::cout << factorial(number) << std::endl;
-  }
+    int total_balls = 0;
+    int drawn_balls = 0;
 
+    cout << "Enter the total number of lottery balls: ";
+    cin >> total_balls;
+    cout << "Enter the number of drawn balls: ";
+    cin >> drawn_balls;
+
+    if (total_balls < 0 or drawn_balls < 0) {
+        cout << "The number of balls must be a positive number." << endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (drawn_balls > total_balls) {
+        cout << "The maxium number of drawn balls is the total amount of balls." << endl;
+        return EXIT_SUCCESS;
+    }
+
+    cout << "The probability of guessing all " << drawn_balls << " balls correctly is 1/"
+        << all_combinations(total_balls, drawn_balls) << endl;
+    return EXIT_SUCCESS;
 }
