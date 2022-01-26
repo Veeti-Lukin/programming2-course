266aad2c40782496d8f41093cde80a45083efc17
Veeti Lukin <veeti.lukin@tuni.fi>
2022-01-26 23:53:13 +0200

    TASK 2.6.1 INITIAL COMMMIT

---
+++ student/02/encryption/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+int main()
+{
+    string encryption_key = "";
+
+    std::cout << "Enter the encryption key: " << std::endl;
+    return EXIT_SUCCESS;
+}
