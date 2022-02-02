ea0c621608b1580f10a227719c2890c0e36ae253
Veeti Lukin <veeti.lukin@tuni.fi>
2022-02-02 21:26:11 +0200

    TASK 3.2.2 ADD FEATURE: for generating and printing random numbers

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -4,18 +4,33 @@
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper)
+void produce_random_numbers(unsigned int lower, unsigned int upper, int seed)
 {
-    // Implement your function here
+    default_random_engine generator(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+
+    while (true) {
+        cout << "\n";
+        cout << "Your drawn random number is " << distr(generator) << endl;
+        cout << "Press q to quit or any other key to continue: ";
+        string answer = "";
+        cin >> answer;
+
+        if (answer == "q")
+            break;
+    }
 }
 
 int main()
 {
     unsigned int lower_bound, upper_bound;
+    int seed;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
     cin >> upper_bound;
+    cout << "Enter a seed value: ";
+    cin >> seed;
 
     if(lower_bound >= upper_bound)
     {
@@ -24,7 +39,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound);
+    produce_random_numbers(lower_bound, upper_bound, seed);
 
     return EXIT_SUCCESS;
 }
