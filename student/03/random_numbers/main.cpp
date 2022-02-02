10e895f15246e81f488a1a13e7fbc7d4fba790a8
Veeti Lukin <veeti.lukin@tuni.fi>
2022-02-02 21:30:26 +0200

    TASK 3.2.2 CODE CLARIFY: relocate user seed input in random_number function

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -4,8 +4,12 @@
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper, int seed)
+void produce_random_numbers(unsigned int lower, unsigned int upper)
 {
+    int seed;
+    cout << "Enter a seed value: ";
+    cin >> seed;
+
     default_random_engine generator(seed);
     uniform_int_distribution<int> distr(lower, upper);
 
@@ -24,13 +28,11 @@ void produce_random_numbers(unsigned int lower, unsigned int upper, int seed)
 int main()
 {
     unsigned int lower_bound, upper_bound;
-    int seed;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
     cin >> upper_bound;
-    cout << "Enter a seed value: ";
-    cin >> seed;
+
 
     if(lower_bound >= upper_bound)
     {
@@ -39,7 +41,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound, seed);
+    produce_random_numbers(lower_bound, upper_bound);
 
     return EXIT_SUCCESS;
 }
