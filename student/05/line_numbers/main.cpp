814cab5ca6585d8182c69edea03f200423ea81ef
Veeti Lukin <veeti.lukin@tuni.fi>
2022-02-21 15:12:58 +0200

    TASK 5.1.1 ADD FEATURE: for reading lines from file

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-using  std::cout, std::cin, std::string, std::ifstream, std::endl,
+using  std::cout, std::cin, std::string, std::ifstream, std::endl, std::vector,
         std::ofstream, std::getline;
+
+vector<string> readLines(ifstream& fileObject){
+    vector<string> lines = {};
+    string line = "";
+    // true if correctly gets line from fileobject
+    while (getline(fileObject, line)){
+        lines.push_back(line);
+    }
+    //when while loop breaks check if reached eof
+    //or if there was error while reading line
+    if (!fileObject.eof()){
+        cout << "There was invalid line in in the file" << endl;
+        return  {};
+    }
+    // no error while reading the file
+    return lines;
+}
 
 int main(){
     cout << "Input file: ";
@@ -11,9 +29,14 @@ int main(){
     getline(cin, fileName);
     ifstream fileObject(fileName);
 
-    if(! fileObject) {
+    if (! fileObject) {
         cout << "ERROR! The file " << fileName << " cannot be opened." << endl;
         return EXIT_FAILURE;
     }
 
+    vector<string> strings = readLines(fileObject);
+    for (string & s : strings){
+        cout << s << endl;
+    }
+    return EXIT_SUCCESS;
 }
