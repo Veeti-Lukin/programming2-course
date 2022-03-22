97aaebcc864d2e7656ca71694a704aa7430b269d
Veeti Lukin <veeti.lukin@tuni.fi>
2022-03-22 20:37:00 +0200

    TASK 8.5.1 ADD FEATURE(class): cards class public interface stub

--- student/08/cards/cards.cpp
+++ student/08/cards/cards.cpp
@@ -1,3 +1,53 @@
 #include "cards.hh"
 
 // TODO: Implement the methods here
+
+Cards::Cards()
+{
+
+}
+
+Card_data *Cards::get_topmost()
+{
+
+}
+
+void Cards::add(int id)
+{
+
+}
+
+void Cards::print_from_top_to_bottom(std::ostream &s)
+{
+
+}
+
+bool Cards::remove(int &id)
+{
+
+}
+
+bool Cards::bottom_to_top()
+{
+
+}
+
+bool Cards::top_to_bottom()
+{
+
+}
+
+void Cards::print_from_bottom_to_top(std::ostream &s)
+{
+
+}
+
+Cards::~Cards()
+{
+
+}
+
+int Cards::recursive_print(Card_data *top, std::ostream &s)
+{
+
+}
