68f5a139847d48bb393acb6523e4668d82e52ce2
Veeti Lukin <veeti.lukin@tuni.fi>
2022-03-29 21:52:40 +0300

    TASK 9.4.1 ADD FEATURE(class): methdo for removing topmost card

--- student/09/reverse/cards.cpp
+++ student/09/reverse/cards.cpp
@@ -24,6 +24,21 @@ void Cards::print(std::ostream& s) {
    }
 }
 
+bool Cards::remove(int &id) {
+    if (top_ == nullptr) {
+        return false;
+    }
+
+    id = top_->data;
+    top_ = top_->next;
+    return true;
+}
+
+void Cards::reverse()
+{
+
+}
+
 // Tip for writing code more efficiently:
 // Do not write the stubs of the methods remove and reverse by yourself here,
 // but open the file cards.hh and click the declaration of the method
