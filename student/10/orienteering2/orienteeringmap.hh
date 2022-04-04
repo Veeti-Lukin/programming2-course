162d16ac9d8391ec779f15d9da1955a2b5746f88
Veeti Lukin <veeti.lukin@tuni.fi>
2022-04-04 16:08:26 +0300

    copy 10th week templates and examples

---
+++ student/10/orienteering2/orienteeringmap.hh
@@ -0,0 +1,72 @@
+/*
+#############################################################################
+# COMP.CS.110 Ohjelmointi 2: Rakenteet / Programming 2: Structures          #
+# Project3: Suunnistus / Orienteering                                       #
+# File: orienteeringmap                                                     #
+# Description: Datastructure that represents an orienteering map and        #
+#        handles information of points and routes                           #
+# Notes: * This is a part of an exercise program.                           #
+#        * Students aren't allowed to change the public interface!          #
+#        * Modifying the private part is allowed (and required).            #
+#############################################################################
+*/
+
+#ifndef ORIENTEERINGMAP_HH
+#define ORIENTEERINGMAP_HH
+
+#include <string>
+
+class OrienteeringMap
+{
+public:
+    // Constructor and destructor
+    OrienteeringMap();
+    ~OrienteeringMap();
+
+    // Sets the width and height for the map.
+    void set_map_size(int width, int height);
+
+    // Adds a new point in the map, with the given name, position (x and y
+    // coordinates), height and marker.
+    void add_point(std::string name, int x, int y, int height, char marker);
+
+    // Connects two existing points such that the point 'to' will be
+    // immediately after the point 'from' in the route 'route_name'.
+    // The given route can be either a new or an existing one,
+    // if it already exists, the connection between points will be
+    // updated in the aforementioned way.
+    // Returns true, if connection was successful, i.e. if both the points
+    // exist, otherwise returns false.
+    bool connect_route(std::string from,
+                       std::string to,
+                       std::string route_name);
+
+    // Prints the map.
+    void print_map() const;
+
+    // Prints all routes' names, ordered alphabetically.
+    void print_routes() const;
+
+    // Prints all points' names and their markers on the map.
+    void print_points() const;
+
+    // Prints points belonging to the given route in the order they are
+    // in the route.
+    void print_route(const std::string& name) const;
+
+    // Prints the given route's combined length,
+    // the length is counted as a sum of the distances of adjacent points.
+    void route_length(const std::string& name) const;
+
+    // Finds and prints the highest rise in any of the routes after the given
+    // point.
+    void greatest_rise(const std::string& point_name) const;
+
+private:
+    // Add here attributes and private methods.
+    // At least you need a datastructure for points or routes or for both,
+    // containing Point* or Route* objects (pointers).
+    // Good candidates for such structures are STL maps or vectors.
+};
+
+#endif // ORIENTEERINGMAP_HH
---
+++ student/10/orienteering2/parser.cpp
@@ -0,0 +1,130 @@
+#include "parser.hh"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+const std::string ERROR_IN_FORMAT = "Error: Incorrect line in input file";
+
+bool read_size(std::ifstream &file, std::shared_ptr<OrienteeringMap> routes);
+bool read_points(std::ifstream &file, std::shared_ptr<OrienteeringMap> routes);
+bool read_routes(std::ifstream &file, std::shared_ptr<OrienteeringMap> routes);
+void split_line(std::vector<std::string>& res, std::string& line);
+bool is_number(std::string& str);
+
+void split_line(std::vector<std::string>& result, std::string& line)
+{
+    result.push_back("");
+    for ( char c : line )
+    {
+        if ( c == ';' )
+        {
+            result.push_back("");
+        }
+        else
+        {
+            result.back() += c;
+        }
+    }
+}
+
+bool is_number(std::string& str)
+{
+    return std::any_of(str.begin(), str.end(), ::isdigit);
+}
+
+bool read_size(std::ifstream& file, std::shared_ptr<OrienteeringMap> routes)
+{
+    std::string line;
+    getline(file, line);
+    std::vector<std::string> temp;
+    split_line(temp, line);
+    if( temp.size() == 2 and is_number(temp.at(0)) and is_number(temp.at(1)) )
+    {
+        routes->set_map_size(std::stoi(temp.at(0)), std::stoi(temp.at(1)));
+        return true;
+    }
+    else
+    {
+        std::cout << ERROR_IN_FORMAT << std::endl;
+        return false;
+    }
+}
+
+bool read_points( std::ifstream& file, std::shared_ptr<OrienteeringMap> routes)
+{
+    std::vector<std::string> temps;
+    std::string line;
+    while( getline( file, line) )
+    {
+        if ( line == "ROUTES" )
+        {
+            return true;
+        }
+        temps.clear();
+        split_line(temps, line);
+        if( temps.size() != 5
+             or not is_number(temps.at(1))
+             or not is_number(temps.at(2))
+             or not is_number(temps.at(3))
+             or temps.at(4).size() != 1 )
+        {
+            std::cout << ERROR_IN_FORMAT << std::endl;
+            return false;
+        }
+        else
+        {
+            routes->add_point(temps.at(0),
+                              std::stoi(temps.at(1)),
+                              std::stoi(temps.at(2)),
+                              std::stoi(temps.at(3)),
+                              temps.at(4).at(0));
+        }
+    }
+    return true;
+}
+
+bool read_routes(std::ifstream& file, std::shared_ptr<OrienteeringMap> routes)
+{
+    std::vector<std::string> temp;
+    std::string line, route;
+    while( getline(file, line) )
+    {
+        temp.clear();
+        split_line(temp, line);
+        if( temp.size() > 2 )
+        {
+            route = temp.at(0);
+            for( uint i = 2 ; i < temp.size(); ++i )
+            {
+                if( not routes->connect_route(temp.at(i - 1),
+                                              temp.at(i),
+                                              route) )
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            std::cout << ERROR_IN_FORMAT << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool read_input_from_file(std::string file,
+                          std::shared_ptr<OrienteeringMap> routes)
+{
+    std::ifstream input(file);
+    if( not input )
+    {
+        std::cout << "Error: Can't read file" << std::endl;
+        return false;
+    }
+    return (read_size(input, routes) and
+            read_points(input, routes) and
+            read_routes(input, routes));
+}
