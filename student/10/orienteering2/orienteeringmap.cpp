2b25e6fdbf40bba0a14d53cfea0d44c4aa55f8f6
Veeti Lukin <veeti.lukin@tuni.fi>
2022-04-05 19:41:39 +0300

    TASK 10.5 ADD FEATURE(class): public interface stub dor OrienteeringMap

--- student/10/orienteering2/orienteeringmap.cpp
+++ student/10/orienteering2/orienteeringmap.cpp
@@ -1,1 +1,56 @@
 #include "orienteeringmap.hh"
+
+OrienteeringMap::OrienteeringMap()
+{
+
+}
+
+OrienteeringMap::~OrienteeringMap()
+{
+
+}
+
+void OrienteeringMap::set_map_size(int width, int height)
+{
+
+}
+
+void OrienteeringMap::add_point(std::string name, int x, int y, int height, char marker)
+{
+
+}
+
+bool OrienteeringMap::connect_route(std::string from, std::string to, std::string route_name)
+{
+
+}
+
+void OrienteeringMap::print_map() const
+{
+
+}
+
+void OrienteeringMap::print_routes() const
+{
+
+}
+
+void OrienteeringMap::print_points() const
+{
+
+}
+
+void OrienteeringMap::print_route(const std::string &name) const
+{
+
+}
+
+void OrienteeringMap::route_length(const std::string &name) const
+{
+
+}
+
+void OrienteeringMap::greatest_rise(const std::string &point_name) const
+{
+
+}
