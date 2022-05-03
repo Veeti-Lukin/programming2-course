17cc1a1f1d835950477146ada0e6aef8abd71cb3
Veeti Lukin <veeti.lukin@tuni.fi>
2022-05-03 22:26:26 +0300

    TASK 12.6 ADD FEATURE: for switching between menu and game pages

--- student/12/numbers_gui/mainwindow.cpp
+++ student/12/numbers_gui/mainwindow.cpp
@@ -11,10 +11,26 @@ MainWindow::MainWindow(QWidget *parent)
     this->setFixedSize(this->width(), this->height());
 
     this->setWindowTitle("2048 Game");
+
+   //connect slots and signals of menupage
+    connect(ui->playButton, &QPushButton::pressed, this, &MainWindow::loadGamePage);
+
+    //connect slots and signals of gamepage
+     connect(ui->menuButton, &QPushButton::pressed, this, &MainWindow::loadMenuPage);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::loadGamePage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->gamePage);
+}
+
+void MainWindow::loadMenuPage()
+{
+    ui->stackedWidget->setCurrentWidget(ui->menuPage);
 }
 
--- student/12/numbers_gui/mainwindow.hh
+++ student/12/numbers_gui/mainwindow.hh
@@ -17,5 +17,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void loadGamePage();
+    void loadMenuPage();
 };
 #endif // MAINWINDOW_HH
