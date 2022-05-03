1eda37f4dc7ae5023525658ff8db0e980104008a
Veeti Lukin <veeti.lukin@tuni.fi>
2022-05-03 20:46:57 +0300

    TASK 12.6 ADD FEATURE(ui): mainmenu page for the game(no functionality yet)

--- student/12/numbers_gui/mainwindow.cpp
+++ student/12/numbers_gui/mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // disable window scaling
+    this->setFixedSize(this->width(), this->height());
+
+    this->setWindowTitle("2048 Game");
 }
 
 MainWindow::~MainWindow()
